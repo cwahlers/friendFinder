6e88ff1edb598ec70ff2b2f455ba9be03cf6b41b
C W <wahlers1@gmail.com>
2017-02-27 18:02:59 -0500

    Home and survey working

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+
+
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends.
+// ===============================================================================
+
+var friendArray = [
+  {
+    name: "Chris F",
+    photoUrl: "https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTIwNjA4NjM0MDYwMTc4OTU2/chris-farley-9542500-1-402.jpg",
+    scores: [1,2,3,4,5,1,2,3,4,5]
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendArray;
