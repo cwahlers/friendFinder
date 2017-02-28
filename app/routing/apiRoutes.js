a08d13f48a2f160d6d1643c08b732ea1b11d0e9d
C W <wahlers1@gmail.com>
2017-02-27 23:18:07 -0500

    With friend found

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -31,10 +31,31 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they successfully posted the survey.
-    console.log("---------------------");
-    console.log(req.body);
+    // console.log("---------------------");
+    // console.log(req.body);
     friends.push(req.body);
-     res.json(true);
+
+    var compareArray = [];
+
+    for (var i = 0; i < friends.length; i++) {
+        var totalDif = 0;
+        if (friends[i].name != req.body.name ) {
+          for (var j = 0; j < 10; j++) {
+            var dif = Math.abs(req.body.scores[j] - friends[i].scores[j]);
+            totalDif+= dif;
+          };
+          compareArray.push({name: friends[i].name,
+                              sum: totalDif,
+                         photoUrl: friends[i].photoUrl});
+        };
+    };
+    compareArray.sort(mycomparator);
+
+    console.log("Your friend: " + compareArray[0].name );
+    var nameObj = {name: compareArray[0].name,
+               photoUrl: compareArray[0].photoUrl }
+    res.json(nameObj);
+
 
   });
 
@@ -48,3 +69,7 @@ module.exports = function(app) {
   //   console.log(friends);
   // });
 };
+
+function mycomparator(a,b) {
+    return parseInt(a.sum, 10) - parseInt(b.sum, 10);
+  }
